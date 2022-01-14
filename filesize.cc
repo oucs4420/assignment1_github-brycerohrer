2136a47b5e59b83d7a5bb418eb360014a243e49e
Brye Rohrer <br269914@ohio.edu>
2022-01-14 10:52:32 -0500

    added final file

--- filesize.cc
+++ filesize.cc
@@ -1,18 +1,29 @@
+// I am enrolled in 4000 and 4420 at the same time, these will be the same files as you said in class we can do that. 
+
 #include <iostream>
+
 #include <fstream>
+
 using namespace std;
 
-// output format to use IDENTICALLY:
-//BSH:Saru> ./filesize input.3lines input.1line 
-//program: ./filesize
-// input.3lines: 3
-// input.1line: 1
+int main(int argc, char * argv[]) {
+  std::cout << "program: " << argv[0] << std::endl; // retrieving the first argument passed, which in this case is the program name
 
-int main( int argc, char* argv[] )
-{
-    // just to get you started, this is how to refer to the arguments that were passed
-    for (int arg = 0; arg < argc; ++arg)
-            std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
+  for (int arg = 1; arg < argc; ++arg) {
+    std::cout << " " << argv[arg] << ":"; // outputting the files passed into the command line
+    ifstream instream(argv[arg]); // creating a stream with the text files
+    if (instream.is_open()) {
+      int numLines = 0;
+      std::string unused;
+      while (std::getline(instream, unused))
+        ++numLines;
 
-    exit(0); // this means that the program executed correctly!
+      std::cout << " " << numLines << std::endl; // keep format consistent with request
+    } else {
+      cout << " " // keep format conistent
+        <<
+        "-1" << endl;
+    }
+  }
+  exit(0); // this means that the program executed correctly!
 }
